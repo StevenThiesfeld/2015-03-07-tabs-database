2c212af76faa0b62b2e95fab4697cca00aab8df3
Steven Thiesfeld <scthiesfeld@hotmail.com>
2015-03-07 18:18:26 -0600

    multi-tabs work now aka I'm awesome

--- public/scripts.js
+++ public/scripts.js
@@ -1,25 +1,32 @@
-var navTabs = [];
-var tabs = [];
+var tabWindows = []
 
 window.onload = function(){
+  //gets all tab windows
+  var tabWindowDivs = document.getElementsByClassName("tab_window");
   
-  var links = document.getElementsByClassName("tab_tag");
-  
-  for (var i = 0; i < links.length; i ++ ) {
-    var id = getHash( links[i].getAttribute ("href") );
-    navTabs[id] = links[i];
-    tabs[id] = document.getElementById(id);
-  }
-  var i = 0;
-  for ( id in navTabs ){
-    navTabs[id].addEventListener("click", swapTab);
-    if (i == 0) navTabs[id].className = "active";
-    i ++;
-  }
-  var i = 0
-  for (var id in tabs){
-    if (i != 0){tabs[id].className = "tab_content hide"}
-    i ++
+  for (var divCounter = 0; divCounter < tabWindowDivs.length; divCounter ++ ){
+    var links = tabWindowDivs[divCounter].getElementsByClassName("tab_tag");
+    var tabWindow = {navTabs: [], tabs: []};
+    for (var i = 0; i < links.length; i ++ ) {
+      var id = getHash( links[i].getAttribute ("href") );
+      tabWindow.navTabs[id] = links[i];
+      tabWindow.tabs[id] = document.getElementById(id);
+    }
+    
+    var i = 0;
+    for ( id in tabWindow.navTabs ){
+      tabWindow.navTabs[id].addEventListener("click", swapTab);
+      if (i == 0) tabWindow.navTabs[id].className = "active";
+      i ++;
+    }
+    
+    var i = 0
+    for (var id in tabWindow.tabs){
+      if (i != 0){tabWindow.tabs[id].className = "tab_content hide"}
+      i ++
+    }
+    
+    tabWindows[tabWindowDivs[divCounter].id] = tabWindow;
   }
 }
 
@@ -27,20 +34,24 @@ window.onload = function(){
 var swapTab = function(e){
   e.preventDefault()
   var selectedId = getHash( this.getAttribute('href') );
-  
-  for (var id in tabs){
+  var tabWindow = tabWindows[getTabWindow(this.getAttribute('href'))]
+  for (var id in tabWindow.tabs){
     if ( id == selectedId ) {
-      navTabs[id].className = 'active';
-      tabs[id].className = 'tab_content';
+      tabWindow.navTabs[id].className = 'active';
+      tabWindow.tabs[id].className = 'tab_content';
     } else {
-      navTabs[id].className = '';
-      tabs[id].className = 'tab_content hide';
+      tabWindow.navTabs[id].className = '';
+      tabWindow.tabs[id].className = 'tab_content hide';
     }
   }
-  return false
 }
 
 function getHash( url ) {
   var hashPos = url.lastIndexOf ( '#' );
   return url.substring( hashPos + 1 );
+}
+
+function getTabWindow( url ) {
+  var windowID = url.substr(url.length - 1);
+  return parseInt(windowID)
 }
