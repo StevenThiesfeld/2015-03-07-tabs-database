3991e516670717dc7658bded4662b1f418f7b3d2
Steven Thiesfeld <scthiesfeld@hotmail.com>
2015-03-09 19:41:30 -0500

    added bundler

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "http://rubygems.org"
+
+gem "sinatra"
+gem "json"
+gem "pry"
+gem "sqlite3"
+gem "activesupport"
--- main.rb
+++ main.rb
@@ -1,10 +1,8 @@
-require "pry"
-require "sqlite3"
+require 'rubygems'
+require 'bundler/setup'
+Bundler.require(:default)
 DATABASE = SQLite3::Database.new("products.db")
-require "active_support/inflector"
 require_relative "models/model_db_methods.rb"
-require "sinatra"
-require "json"
 require_relative "database/database-setup.rb"
 require_relative "models/product.rb"
 
